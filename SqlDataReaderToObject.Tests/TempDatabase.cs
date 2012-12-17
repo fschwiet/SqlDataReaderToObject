6af7d0b3a0174ba95dcb6b5315e0a52f1bc645dd
fschwiet <fschwiet@gmail.com>
2012-12-16 17:55:44 -0800

    first passing test- can create test database

---
+++ SqlDataReaderToObject.Tests/TempDatabase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDataReaderToObject.Tests
+{
+    public class TempDatabase : IDisposable
+    {
+        public string File { get; private set; }
+        public string ConnectionString { get; private set; }
+        private List<IDbConnection> _connections = new List<IDbConnection>(); 
+
+        public static TempDatabase Create()
+        {
+            var file = Path.GetTempFileName();
+
+            var connectionString = new SqlConnectionStringBuilder();
+            connectionString.DataSource = file;
+            connectionString.PersistSecurityInfo = false;
+
+            
+            SqlCeEngine en = new SqlCeEngine(connectionString.ToString());
+            return new TempDatabase() { File = file, ConnectionString = en.LocalConnectionString };
+        }
+
+        private TempDatabase()
+        {
+        }
+
+        public IDbConnection GetConnection()
+        {
+            var result = new SqlCeConnection(ConnectionString);
+            _connections.Add(result);
+            return result;
+        }
+
+        public void Dispose()
+        {
+            foreach (var connection in _connections)
+            {
+                connection.Close();
+            }
+            
+            new FileInfo(File).Delete();
+        }
+    }
+}
---
+++ SqlDataReaderToObject.Tests/TempDatabaseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SqlDataReaderToObject.Tests
+{
+    [TestFixture]
+    public class TempDatabaseTest
+    {
+        [Test]
+        public void ShouldBeAbleToCreateTestDatabase()
+        {
+            using (var database = TempDatabase.Create())
+            {
+                using (var connection = database.GetConnection())
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"CREATE TABLE Foo([column1] [bigint] NOT NULL)";
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                using (var connection = database.GetConnection())
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"SELECT COUNT(*) + 123 FROM Foo";
+                        var result = (int)command.ExecuteScalar();
+                        Assert.That(result, Is.EqualTo(123));
+                    }
+                }
+
+                // leaked connections are ok
+                database.GetConnection().Open();
+                database.GetConnection();
+            }
+        }
+    }
+}
