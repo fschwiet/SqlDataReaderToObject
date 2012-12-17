6f1376b2fefed02329fe07414490d143d8b49350
fschwiet <fschwiet@gmail.com>
2012-12-16 18:18:53 -0800

    can hydrate first object

--- SqlDataReaderToObject/SqlMapper.cs
+++ SqlDataReaderToObject/SqlMapper.cs
@@ -9,12 +9,33 @@ namespace SqlDataReaderToObject
 {
     public class SqlMapper
     {
-        public T ReadObject<T>(IDataReader reader)
+        public T ReadObject<T>(IDataReader reader) where T : new()
         {
-            throw new NotImplementedException();
+            var type = typeof (T);
+            Dictionary<string,Action<T,object>> setters = new Dictionary<string, Action<T, object>>();
+
+            foreach (var field in type.GetFields().Where(f => f.IsPublic))
+            {
+                setters[field.Name] = (t, v) => field.SetValue(t, v);
+            }
+
+            T result = new T();
+
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                var name = reader.GetName(i);
+                Action<T, object> setter;
+
+                if (setters.TryGetValue(name, out setter))
+                {
+                    setter(result, reader[i]);
+                }
+            }
+
+            return result;
         }
 
-        public IEnumerable<T> ReadObjects<T>(IDataReader reader)
+        public IEnumerable<T> ReadObjects<T>(IDataReader reader) where T : new()
         {
             List<T> results = new List<T>();
             while (reader.Read())
